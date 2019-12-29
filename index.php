358c6eeeb49646a2f4490cac3f235b680bd5b0d2
samkent <kent_sam@outlook.com>
2019-12-29 17:24:12 +0000

    Added structure of the site.

---
+++ index.php
@@ -0,0 +1,46 @@
+<head>
+    <!--
+        Site written by Samuel Kent as a personal site.
+        Started on the 29th December 2019.
+        (Unfinished)
+    -->
+
+    <!-- Tab title and icon. -->
+    <title>Sam Antics</title>
+    <link rel="icon" href="Images/Icon.png">
+
+    <!-- Meta Information for the site. -->
+    <meta name="author" content="Samuel Kent">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+    <!-- Link to the stylesheet and javascript. -->
+    <link rel="stylesheet" href="styles.css">
+    <script type="text/javascript" src="script.js"></script>
+</head>
+
+<!-- All the displayed HTML is in here -->
+
+<body>
+    <header>
+        Sam Antics
+    </header>
+
+    <nav>
+       <ul>
+           <li onclick="window.location.href = '#about';">About Me</li>
+           <li onclick="window.location.href = '#videos';">Personal Projects</li>
+           <li onclick="window.location.href = '#art';">University Work</li>
+       </ul>
+    </nav>
+
+    <main>
+
+    </main>
+
+    <footer>
+        
+    </footer>
+</body>
+
+</html>
